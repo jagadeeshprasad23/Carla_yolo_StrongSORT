729a2a196cc0198b8dd0b1b51c300643c062e3d3
Jagadeesh Prasad <jash1691@gmail.com>
2023-07-26 16:07:31 +0100

    Update yolo.py

--- yolo_custom_detection_training/yolo.py
+++ yolo_custom_detection_training/yolo.py
@@ -1,1 +1,15 @@
+from ultralytics import YOLO
+import torch
 
+# Load a model
+model = YOLO("yolov8s.yaml") 
+
+# Use the model
+def run():
+    model.train(data="data.yaml", epochs=1000)  # train the model
+
+# Ensure CUDA is available
+if torch.cuda.is_available():
+    run()
+else:
+    print("CUDA is not available on this device.")

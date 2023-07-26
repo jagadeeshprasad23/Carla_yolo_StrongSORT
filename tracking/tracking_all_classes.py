ef1a5bdddb7cd57afbf601da581735209886f2d8
Jagadeesh Prasad <jash1691@gmail.com>
2023-07-26 22:22:37 +0100

    Updated Tracker

--- tracking/tracking_all_classes.py
+++ tracking/tracking_all_classes.py
@@ -1,48 +1,49 @@
+from typing import Any
 import torch
 import numpy as np
 import cv2
 from time import perf_counter
 from ultralytics import YOLO
+import yaml
 from pathlib import Path
 import supervision as sv
 from types import SimpleNamespace
 import carla
+import math
 import random
 import time
-import yaml
-from strongsort.strong_sort import StrongSORT
+from strongsort.strong_sort import StrongSORT        
 
 SAVE_VIDEO = True
 TRACKER = 'strongsort'
 IM_WIDTH = 256*4
 IM_HEIGHT = 256*3
-MODEL_PATH = 'weights/yolov8n.pt'
 
 class ObjectTracking:
     def __init__(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print('using device:', self.device)
-
+        print('using device: ', self.device)
+        
         self.model = self.load_model()
         self.CLASS_NAMES_DICT = self.model.model.names
-
+        
         self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
-
+        
         # reid weights
         reid_weights = Path('weights/osnet_x0_25_msmt17.pt')
-
+        
         if SAVE_VIDEO:
             self.video_save_path = "output_video.mp4"
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
             self.video_writer = cv2.VideoWriter(self.video_save_path, fourcc, 20.0, (IM_WIDTH, IM_HEIGHT))
-
+        
         if TRACKER == 'strongsort':
             print('Tracker = Strongsort')
-            tracker_config = 'configs/strongsort.yaml'
+            tracker_config = 'configs/strongsort.yaml' 
             with open(tracker_config, 'r') as file:
                 cfg = yaml.load(file, Loader=yaml.FullLoader)
             cfg = SimpleNamespace(**cfg)
-
+            
             self.tracker = StrongSORT(
                 reid_weights,
                 torch.device(self.device),
@@ -56,50 +57,16 @@ class ObjectTracking:
                 mc_lambda=cfg.mc_lambda,
                 ema_alpha=cfg.ema_alpha,
             )
-
     def load_model(self):
-        # Load the YOLO model
-        model = YOLO(MODEL_PATH)
+        model = YOLO('yolov8n.pt')
+        
         return model
 
-    def draw_results(self, frame, results):
-        xyxys = []
-        confidences = []
-        class_ids = []
-        detections = []
-        boxes = []
-
-        for result in results:
-            class_id = result.names[0]
-
-            if class_id not in self.CLASS_NAMES_DICT:
-                continue
-
-            xyxys.append(result.xyxy[0].cpu().numpy())
-            confidences.append(result.conf[0].cpu().numpy())
-            class_ids.append(self.CLASS_NAMES_DICT[class_id])
-            boxes.append(result)
-
-            detections.append(
-                sv.Detections(
-                    xyxy=result.xyxy[0].cpu().numpy(),
-                    confidence=result.conf[0].cpu().numpy(),
-                    class_id=self.CLASS_NAMES_DICT[class_id]
-                )
-            )
-
-        # format custom labels
-        self.labels = [f'{class_id} {confidences[i]:0.2f}' for i, class_id in enumerate(class_ids)]
-
-        # annotate and display frame
-        frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
-        return frame, boxes
-
-    def run_tracking(self):
+    def __call__(self):
         # The local Host for carla simulator is 2000
         client = carla.Client('localhost', 2000)
         client.set_timeout(20.0)
-        world = client.get_world()
+        world = client.get_world() 
 
         # blueprint will access to all blueprints to create objects (vehicles, people, etc.)
         bp_lib = world.get_blueprint_library()
@@ -116,11 +83,13 @@ class ObjectTracking:
         for i in range(spawn_num):
             vehicle_bp = random.choice(bp_lib.filter('vehicle'))
             npc = world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
-
+            
         for v in world.get_actors().filter('*vehicle*'):
             v.set_autopilot(True)
-
+            
         camera_bp = bp_lib.find('sensor.camera.rgb')
+        
+
         camera_bp.set_attribute('image_size_x', f'{IM_WIDTH}')
         camera_bp.set_attribute('image_size_y', f'{IM_HEIGHT}')
         camera_bp.set_attribute('fov', '110')
@@ -140,40 +109,58 @@ class ObjectTracking:
         camera.listen(lambda image: camera_callback(image, camera_data))
 
         vehicle.set_autopilot(True)
-
+        
         curr_frames, prev_frames = None, None
-
+        
+        #model = YOLO('yolov8n.pt')
+        details = []
+        
         while True:
+            print('inside while')
             start_time = perf_counter()
             frame = camera_data['image']
-            results = self.model.predict(frame)
-            frame, boxes = self.draw_results(frame, results)
+            results = self.model(frame)
+            
+            for box in results:  
+                for r in box.boxes.data.tolist():
+                    x1, y1, x2, y2, conf, id = r
+                    x1 = torch.tensor(x1)
+                    y1 = torch.tensor(y1)
+                    x2 = torch.tensor(x2)
+                    y2 = torch.tensor(y2)
+                    conf = torch.tensor(conf)
+                    details.append([x1.cpu().numpy(), y1.cpu().numpy(), x2.cpu().numpy(), y2.cpu().numpy(), conf.cpu().numpy(),id])
+                    
+                    #details.append([int(x), int(y), int(w), int(h), conf,id])
 
-            if hasattr(self, 'tracker') and hasattr(self.tracker, 'tracker'):
-                if prev_frames is not None and curr_frames is not None:
+            np_details = np.array(details)
+            print(np_details)
+
+            if hasattr(self.tracker, 'tracker'):
+                if prev_frames in locals() and prev_frames is not None and curr_frames in locals() and curr_frames is not None:
                     self.tracker.tracker.camera_update(prev_frames, curr_frames)
 
             outputs = [None]
-            for result in boxes:
-                outputs[0] = self.tracker.update(result, frame)
-                for i, output in enumerate(outputs[0]):
-                    bbox = output[0:4]
-                    tracked_id = output[4]
-                    top_left = (int(bbox[-2] - 100), int(bbox[1]))
-                    cv2.putText(frame, f"ID: {tracked_id}", top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+            #for result in boxes:
+            #for detail in np_details:
+            outputs[0] = self.tracker.update(np_details, frame)
+            for i, output in enumerate(outputs[0]):
+                bbox = output[0:4]
+                tracked_id = output[4]
+                top_left = (int(bbox[-2] - 100), int(bbox[1]))
+                cv2.putText(frame, f"ID: {tracked_id}", top_left, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
             end_time = perf_counter()
             fps = 1 / np.round(end_time - start_time, 2)
 
+            # Convert the frame to a compatible format (e.g., cv::UMat) if needed
+            frame = cv2.UMat(frame)
+
             cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
             cv2.imshow('Yolov8 StrongSORT', frame)
 
-            if SAVE_VIDEO:
-                self.video_writer.write(frame)
-
             if cv2.waitKey(1) == ord('q'):
                 break
-
         if SAVE_VIDEO:
             self.video_writer.release()
 
@@ -183,4 +170,4 @@ class ObjectTracking:
         vehicle.destroy()
 
 tracker = ObjectTracking()
-tracker.run_tracking()
+tracker()
